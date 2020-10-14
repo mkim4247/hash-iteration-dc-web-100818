fcd9d717b588fee58eb19a680f1ae7427cde8794
Michael Kim <m.kim4247@gmail.com>
2020-10-14 01:25:08 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -6,7 +6,7 @@
 # suite_e: "Crumpet the Elf"
 # }
 
-def select_winner(passengers)
+def select_winner(passenger)
   winner = ''
   passenger.each do |suite, name|
     if suite == :suite_a && name.start_with?("A")
